4f8479bf67340d306094e6a7cce58f3a51018341
ssinghravan <ssinghr100@gmil.com>
2026-04-01 23:12:44 +0530

    updated api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://dream-interpreter-dclp.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 30000,
 });
